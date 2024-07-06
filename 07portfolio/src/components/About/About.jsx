292acede7ef3d1ebf1eb21ac9d4ad6e73bbe6b70
bhumika1331 <sharmabhumika54@gmail.com>
2024-07-06 21:52:58 +0530

    portfolio updated as per tzar tea

--- 07portfolio/src/components/About/About.jsx
+++ 07portfolio/src/components/About/About.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
+import tzar_1 from '../../assets/Images/tzar_1.png';
 
 export default function About() {
     return (
-        <div className="py-16 bg-white">
+        <div className="py-16 bg-gradient-to-r from-black to-green-900">
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                    <div className="md:5/12 lg:w-5/12">
+                    <div className="md:5/12 lg:w-5/12 ">
                         <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            src={tzar_1}
                             alt="image"
+                            className='rounded-lg'
                         />
                     </div>
                     <div className="md:7/12 lg:w-6/12">
-                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                            React development is carried out by passionate developers
+                        <h2 className="text-2xl text-Golden font-semi-bold md:text-4x ">
+                        
+                        The 'art of tea' is a spiritual force for us to share...
                         </h2>
-                        <p className="mt-6 text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                        <p className="mt-6 text-yellow-100">
+                        Sip the taste of premium quality tea of Assam, a perfect blend of true essence and flavour.
+                         Tzar brand presents The Classic Chai - Available on Amazon and Flipkart.
                         </p>
-                        <p className="mt-4 text-gray-600">
-                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                        <p className="mt-4 text-yellow-100">
+                        Tzar Tea dedicates to source, process Premium Quality Handpicked Tea from the Finest 
+                        Tea-estates of India & HandCrafted for Great Taste.
                         </p>
                     </div>
                 </div>
--- 07portfolio/tailwind.config.js
+++ 07portfolio/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Golden: '#F7DC6F'
+      }
+    },
   },
   plugins: [],
 }
